d2bdbcca4e87147402666b69bd03b9e4495a3ddf
aric08 <aricmar26@crosspointacademy.org>
2025-10-27 16:00:08 +0000

    pokemon

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         Pokemon p1 = new Pokemon("Tauros", 100, "Body Slam", "Earthquake", "Blizzard", "Hyper Beam");
         Pokemon p2 = new Pokemon("Snorlax", 99, "Fire Blast", "Surf", "Rest", "Double-Edge");
         Pokemon p3 = new Pokemon("Starmie", 98, "Thunderbolt", "Ice Beam", "Recover", "Psychic");
@@ -8,11 +7,11 @@ public class Main {
         Pokemon p5 = new Pokemon("Alakazam", 96, "Seismic Toss", "Reflect", "Thunder Wave", "Kinesis");
         Pokemon p6 = new Pokemon("Lapras", 95, "Sing", "Confuse Ray", "Surf", "Body Slam");
 
-        p1.attack1();
-        p2.attack1();
-        p3.attack1();
-        p4.attack1();
-        p5.attack1();
-        p6.attack1();
+        p1.printPokemon();
+        p2.printPokemon();
+        p3.printPokemon();
+        p4.printPokemon();
+        p5.printPokemon();
+        p6.printPokemon();
     }
 }
--- Pokemon.java
+++ Pokemon.java
@@ -2,38 +2,28 @@ public class Pokemon {
 
     String name;
     int level;
+    String move1;
+    String move2;
+    String move3;
+    String move4;
 
-    String attack1;
-    String attack2;
-    String attack3;
-    String attack4;
-
-    Pokemon() {
-        level = 1;
-    }
-
-    Pokemon(String name, int level, String attack1, String attack2, String attack3, String attack4) {
+    
+    public Pokemon(String name, int level, String m1, String m2, String m3, String m4) {
         this.name = name;
         this.level = level;
-        this.attack1 = attack1;
-        this.attack2 = attack2;
-        this.attack3 = attack3;
-        this.attack4 = attack4;
+        this.move1 = m1;
+        this.move2 = m2;
+        this.move3 = m3;
+        this.move4 = m4;
     }
 
-    void attack1() {
-        System.out.println(name + " attacked with " + attack1 + "!");
-    }
-
-    void attack2() {
-        System.out.println(name + " attacked with " + attack2 + "!");
-    }
-
-    void attack3() {
-        System.out.println(name + " attacked with " + attack3 + "!");
-    }
-
-    void attack4() {
-        System.out.println(name + " attacked with " + attack4 + "!");
+    
+    public void printPokemon() {
+        System.out.println(name + " (Level " + level + ")");
+        System.out.println("1) " + move1);
+        System.out.println("2) " + move2);
+        System.out.println("3) " + move3);
+        System.out.println("4) " + move4);
+        System.out.println();
     }
 }
